a9e0dbb0cd92e7ebc38da72025d070f3c589deef
tonmoy <tonmoy3673@gmail.com>
2023-12-10 02:32:24 +0600

    set spinner

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import { ToastContainer } from "react-toastify";
 
 const Main = () => {
+  const navigation = useNavigation();
   return (
     <div>
       <Navbar />
       <Outlet />
+      <div
+        className={navigation.state === "loading" ? <p>Loading ...</p> : null}
+      ></div>
       <ToastContainer />
     </div>
   );
