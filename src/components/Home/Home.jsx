5e666807c101fdcda16e5d6fd677e97ae6d55f6a
tonmoy <tonmoy3673@gmail.com>
2023-12-10 00:47:51 +0600

    design home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,17 +7,21 @@ const Home = () => {
 
   return (
     <div>
-      <h1 className="text-center text-sky-700 font-semibold py-4">
+      <h1 className="text-center text-sky-700 font-semibold py-4 text-2xl">
         Total T-shirts : <span>{tshirts.length} pics</span>
       </h1>
       <div className="flex">
-        <div>
+        <div className="py-10 grid md:grid-cols-3 grid-cols-1 gap-10 px-16 w-9/12">
           {tshirts.map((tshirt) => (
             <Products key={tshirt._id} tshirt={tshirt} />
           ))}
         </div>
-        <div>
-          <h2>Order Details</h2>
+        <div className="relative w-3/12 text-center">
+          <div className=" py-10 sticky top-0">
+            <h2 className="text-xl text-sky-800 font-semibold">
+              Order Details
+            </h2>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Home/Products.jsx
+++ src/components/Home/Products.jsx
@@ -3,9 +3,18 @@ const Products = (tshirt) => {
   const { name, picture, price, _id } = tshirt.tshirt;
   return (
     <div>
-      <img src={picture} alt="photo" />
-      <h2>{name}</h2>
-      <h2>{price}</h2>
+      <img src={picture} alt="photo" className="w-52 rounded-md h-56 mx-auto" />
+      <h2 className="text-center py-2 text-lg text-blue-500 font-semibold">
+        {name}
+      </h2>
+      <h2 className="text-center ">
+        Price : <span className="text-red-500">{price}$</span>
+      </h2>
+      <div className="py-3 text-center">
+        <button className="bg-blue-500 py-2 px-3 hover:bg-orange-500 rounded-lg">
+          Buy Now
+        </button>
+      </div>
     </div>
   );
 };
